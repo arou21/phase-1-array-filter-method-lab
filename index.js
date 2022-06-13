6fb2afe8d0d8914e60a0a47d31928ce0a6330cf6
Samuel <smarouba2@gmail.com>
2022-06-13 17:38:30 -0400

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+
+// * Es2015 implementation 
+let drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sara']
+ function findMatching(arr, query) {
+    return arr.filter(function (el) {
+        return el.toLowerCase().indexOf(query.toLowerCase())
+        !== -1
+    })
+ }
+console.log(findMatching(drivers, 'bo'))
+console.log(findMatching(drivers, 'sa'))
+
+function fuzzyMatch(arr, query) {
+  return arr.filter (function (el) {
+    return el.substring(0,2).toLowerCase().indexOf(query.toLowerCase())
+        !== -1
+})
+}
+
+function matchName(arrayOfObjects, val) {
+  const reslut = arrayOfObjects.filter(object => object.name === val)
+return reslut
+}
